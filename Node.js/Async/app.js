7f3bda9557779412a2d2e6d978291704af560e7c
Thitikorn Wachiraarunwong <thikon@gmail.com>
2019-05-16 22:27:57 +0700

    async/await sample (single and parallel)

---
+++ Node.js/Async/app.js
@@ -0,0 +1,60 @@
+function sayHello(){
+    return 'hello';
+}
+
+function delaySayHello(){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve('inside hello');
+        }, 1000);
+
+    } );
+}
+
+async function longTimeHello(){
+    await setTimeout(() => {}, 1000);
+    return 'Long Time Hello';
+}
+
+/* basic calling */
+
+// function main(){
+//     let a = sayHello();
+//     let b = delaySayHello();
+
+//     console.log(a);
+//     console.log(b);
+// }
+
+/* calling solve could not extract resolve(object) for callback */
+
+// function main(){
+//     let a = sayHello();
+//     delaySayHello().then( (value) =>{
+//         let b = value;
+//         console.log(b);
+//     });
+//     console.log(a);
+// }
+
+/* main calling top to down */
+
+async function main(){
+    let a = sayHello();
+    let b = await delaySayHello(); // <- with promise inside function
+    let c = await longTimeHello(); // <- without promise function
+
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+
+//main();
+
+/* main calling parallelRun */
+async function parallelRun(){
+    let a = await Promise.all([delaySayHello(),longTimeHello()]);
+    console.log(a);
+}
+
+parallelRun();
